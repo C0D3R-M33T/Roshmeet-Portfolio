6e825c0be65286cc831081ee1c059b8d68c4a3d7
Roshmeet Chakraborty <riju17chak@gmail.com>
2023-04-18 01:23:30 +0530

    contact page updated

--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -7,7 +7,7 @@ function Contactpage() {
     <div className='contactbackground'>
       <Container>
         <h2 className='contacthead'>Get In Touch</h2>
-        <p className='contactpara'> I'm currently looking for short period Internship offers. <br /> If there is any vacancy my inbox is always open. Whether <br /> Any further queries about me and my skills, feel free to get in touch. <br /> I’ll try my best to get back to you!</p>
+        <p className='contactpara'> I'm currently looking for short period Internship offers. <br /> If you want your job done, just drop a dm in my whatsapp inbox<br /> Any further queries about me and my skills, feel free to get in touch. <br /> I’ll try my best to get back to you!</p>
         <button
           className='contactbtn'
           onClick={() => {
